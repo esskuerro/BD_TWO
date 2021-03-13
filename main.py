21a12eaccdc4de9762854fdf9ce3f71aaa7c24f2
esskuerro <79753458+esskuerro@users.noreply.github.com>
2021-03-13 18:51:21 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 import random
+import os
+
+c = os.walk("BD")
+for i in c:
+    print(f"{i[2]}\n")
+    for j in i[2]:
+        print(j)
 
 for i in range(20):
-    f = open(f"BD/{i}", 'w')
+    f = open(f"BD/{random.randint(0, 100)}", 'w')
     f.write(f"{random.randint(1, 1000)}")
     f.close()
+    summa = 0
+
+# for i in range(20):
+#    f = open(f"BD/{i}", 'r')
+#    a = int(f.read())
+#    summa = summa + a
+#    f.close()
+#print(summa)
+
+#for i in range():
+#    os.remove(f"BD/{i}")
